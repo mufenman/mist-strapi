8ac74ac1022e752403b59335afb2f401fcd4b236
Chupryna Dmytro <dchup@softserveinc.com>
2022-04-29 13:29:57 +0300

    test connect db postgres

--- config/database.js
+++ config/database.js
@@ -1,41 +1,39 @@
-const path = require("path");
-
-module.exports = ({ env }) => {
-  console.log(env("DATABASE_HOST"));
-  console.log(env("DATABASE_PORT"));
-  return {
-    connection: {
-      client: "sqlite",
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-};
+// const path = require("path");
 
 // module.exports = ({ env }) => {
+//   console.log(env("DATABASE_HOST"));
+//   console.log(env("DATABASE_PORT"));
 //   return {
 //     connection: {
-//       client: "postgres",
+//       client: "sqlite",
 //       connection: {
-//         host: env("DATABASE_HOST", "127.0.0.1"),
-//         port: env.int("DATABASE_PORT", 5432),
-//         database: env("DATABASE_NAME", "strapi"),
-//         user: env("DATABASE_USERNAME", "strapi"),
-//         password: env("DATABASE_PASSWORD", "strapi"),
-//         ssl: {
-//           ca: env("DATABASE_CA"),
-//         },
+//         filename: path.join(
+//           __dirname,
+//           "..",
+//           env("DATABASE_FILENAME", ".tmp/data.db")
+//         ),
 //       },
-//       debug: false,
+//       useNullAsDefault: true,
 //     },
 //   };
 // };
+
+module.exports = ({ env }) => {
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: true,
+      },
+      debug: false,
+    },
+  };
+};
 
 // host     : app-6b4fe47a-8a0f-44d2-bab9-0f01d928c193-do-user-5051522-0.b.db.ondigitalocean.com
 // port     : 25060
