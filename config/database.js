c22a22db2f8ad4202cd23d1b643f9ae3b427fc6e
Chupryna Dmytro <dchup@softserveinc.com>
2022-04-29 19:10:23 +0300

    test posts

--- config/database.js
+++ config/database.js
@@ -23,11 +23,14 @@ module.exports = ({ env }) => {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        host: env(
+          "DATABASE_HOST",
+          "app-6b4fe47a-8a0f-44d2-bab9-0f01d928c193-do-user-5051522-0.b.db.ondigitalocean.com"
+        ),
+        port: env.int("DATABASE_PORT", 25060),
+        database: env("DATABASE_NAME", "db"),
+        user: env("DATABASE_USERNAME", "db"),
+        password: env("DATABASE_PASSWORD", "96XFfG4aOxEB5O94"),
         ssl: {
           rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
         },
