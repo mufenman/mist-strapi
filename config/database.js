dc9905feb6790a25b382c23d0c7b0d401416ec11
Chupryna Dmytro <dchup@softserveinc.com>
2022-04-29 13:14:35 +0300

    test

--- config/database.js
+++ config/database.js
@@ -1,11 +1,44 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  console.log(env("DATABASE_HOST"));
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
+
+// module.exports = ({ env }) => {
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host: env("DATABASE_HOST", "127.0.0.1"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_NAME", "strapi"),
+//         user: env("DATABASE_USERNAME", "strapi"),
+//         password: env("DATABASE_PASSWORD", "strapi"),
+//         ssl: {
+//           ca: env("DATABASE_CA"),
+//         },
+//       },
+//       debug: false,
+//     },
+//   };
+// };
+
+// host     : app-6b4fe47a-8a0f-44d2-bab9-0f01d928c193-do-user-5051522-0.b.db.ondigitalocean.com
+// port     : 25060
+// username : db
+// password : 96XFfG4aOxEB5O94
+// database : db
+// sslmode  : require
