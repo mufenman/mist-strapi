a31d067ddd8e8bb264fe2462223c44787d254871
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-06 21:04:42 +0300

    add events and categories

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  *  event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController("api::event.event", () => ({
+  async find(ctx) {
+    ctx.query = { ...ctx.query, populate: "*" };
+
+    const res = await super.find(ctx);
+    return res;
+  },
+
+  async findOne(ctx) {
+    ctx.query = { ...ctx.query, populate: "*" };
+
+    const res = await super.find(ctx);
+    return res;
+  },
+}));
