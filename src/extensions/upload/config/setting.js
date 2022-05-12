110da0cdcb7af01cdf5401f2c4150a5e3735cb79
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-12 13:26:59 +0300

    add image upload

---
+++ src/extensions/upload/config/setting.js
@@ -0,0 +1,9 @@
+module.exports = {
+  provider: "do",
+  providerOptions: {
+    key: env("DO_SPACE_ACCESS_KEY"),
+    secret: env("DO_SPACE_SECRET_KEY"),
+    endpoint: env("DO_SPACE_ENDPOINT"),
+    space: env("DO_SPACE_BUCKET"),
+  },
+};
