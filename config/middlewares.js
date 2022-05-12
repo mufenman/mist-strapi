99ccadcc8e8ca199e7aaddc87abe9b07c5690449
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-12 17:39:36 +0300

    csp

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "img-src": ["'self'", "data:", env("DO_SPACE_ENDPOINT")],
+        },
+      },
+    },
+  },
+  ,
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
