4ef7ca53bc0e4dd583a71a4e7df28431ddad6499
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-12 15:12:29 +0300

    add image upload

--- src/api/footer/controllers/footer.js
+++ src/api/footer/controllers/footer.js
@@ -6,9 +6,35 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::footer.footer", () => ({
+module.exports = createCoreController("api::footer.footer", ({ strapi }) => ({
   async find(ctx) {
-    ctx.query = { ...ctx.query, populate: "*" };
+    const populate = [
+      "Social",
+      "logo",
+      "Menu",
+      "Partner",
+      "Contacts",
+      "AntiCorruption",
+    ];
+    ctx.query = {
+      ...ctx.query,
+      populate: populate.reduce((a, v) => {
+        if (v === "logo") {
+          return {
+            ...a,
+            [v]: {
+              populate: "attributes",
+            },
+          };
+        }
+        return {
+          ...a,
+          [v]: {
+            populate: "*",
+          },
+        };
+      }, {}),
+    };
 
     const res = await super.find(ctx);
     return res;
