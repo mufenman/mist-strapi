b4674679379332b46c958d86a04f03ccf6704a1d
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-16 15:27:38 +0300

    add events api

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -8,8 +8,31 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::event.event", () => ({
   async find(ctx) {
-    ctx.query = { ...ctx.query, populate: "*" };
-
+    const populate = ["places", "events_category"];
+    ctx.query = {
+      ...ctx.query,
+      populate: populate.reduce((a, v) => {
+        if (v === "events_category") {
+          return {
+            ...a,
+            [v]: {
+              populate: {
+                data: {
+                  fields: ["Title"],
+                },
+              },
+            },
+          };
+        }
+        return {
+          ...a,
+          [v]: {
+            populate: "*",
+          },
+        };
+      }, {}),
+    };
+    // ctx.query = { ...ctx.query, populate: "*" };
     const res = await super.find(ctx);
     return res;
   },
