be6957f31e666af5ba6a164258d48bd8f48ae62a
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-19 12:33:22 +0300

    add  homePage Controller and cover image for events

--- src/api/home-page/controllers/home-page.js
+++ src/api/home-page/controllers/home-page.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  *  home-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home-page.home-page');
+module.exports = createCoreController("api::home-page.home-page", () => ({
+  async find(ctx) {
+    const populate = ["Events"];
+
+    ctx.query = {
+      ...ctx.query,
+      populate: populate.reduce((a, v) => {
+        return {
+          ...a,
+          [v]: {
+            populate: "*",
+          },
+        };
+      }, {}),
+    };
+
+    const res = await super.find(ctx);
+    return res;
+  },
+}));
