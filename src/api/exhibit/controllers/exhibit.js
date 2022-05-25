7d215a3f172534ee94a0e8bd4d943a1aa547f426
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-25 14:12:01 +0300

    exhibit added

---
+++ src/api/exhibit/controllers/exhibit.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ *  exhibit controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::exhibit.exhibit", () => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        titleImage: {
+          fields: ["url", "name"],
+        },
+      },
+    };
+    const res = await super.find(ctx);
+    return res;
+  },
+
+  async findOne(ctx) {
+    ctx.query = { ...ctx.query, populate: "*" };
+
+    const res = await super.find(ctx);
+    return res;
+  },
+}));
