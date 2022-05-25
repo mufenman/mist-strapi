396d11b737994f73dae9f8ea232d4adeb577e28d
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-25 13:28:03 +0300

    post cats

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -8,40 +8,18 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::post.post", () => ({
   async find(ctx) {
-    const populate = ["titleImage", "category_posts"];
-    // ctx.query = {
-    //   ...ctx.query,
-    //   populate: populate.reduce((a, v) => {
-    //     if (v === "category_posts") {
-    //       return {
-    //         ...a,
-    //         [v]: {
-    //           populate: {
-    //             data: {
-    //               fields: ["Title"],
-    //             },
-    //           },
-    //         },
-    //       };
-    //     }
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        titleImage: {
+          fields: ["url", "name"],
+        },
+        category_post: {
+          fields: ["title"],
+        },
+      },
+    };
 
-    //     if (v === "titleImage") {
-    //       return {
-    //         ...a,
-    //         [v]: {
-    //           fields: ["url", "name"],
-    //         },
-    //       };
-    //     }
-
-    //     return {
-    //       ...a,
-    //       [v]: {
-    //         populate: "*",
-    //       },
-    //     };
-    //   }, {}),
-    // };
     const res = await super.find(ctx);
     return res;
   },
