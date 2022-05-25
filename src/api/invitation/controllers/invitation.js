c73c082d58ed4993335597631a03678669394c9b
Chupryna Dmytro <dchup@softserveinc.com>
2022-05-25 13:46:24 +0300

    invitation address

--- src/api/invitation/controllers/invitation.js
+++ src/api/invitation/controllers/invitation.js
@@ -11,7 +11,7 @@ module.exports = createCoreController("api::invitation.invitation", () => ({
     ctx.query = {
       ...ctx.query,
       populate: {
-        places: {
+        address: {
           populate: "*",
         },
         titleImage: {
