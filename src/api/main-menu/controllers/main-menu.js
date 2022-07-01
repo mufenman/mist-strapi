f179ab1be3a8303ab8f7c6e0fc3f2ec7848c0a0f
Chupryna Dmytro <dchup@softserveinc.com>
2022-07-01 13:24:29 +0300

    add main menu

---
+++ src/api/main-menu/controllers/main-menu.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ *  main-menu controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::main-menu.main-menu", () => ({
+  async find(ctx) {
+    const populate = ["menu", "logo"];
+    ctx.query = {
+      ...ctx.query,
+      populate: populate.reduce((a, v) => {
+        if (v === "menu") {
+          return {
+            ...a,
+            [v]: {
+              populate: "*",
+            },
+          };
+        }
+        if (v === "logo") {
+          return {
+            ...a,
+            [v]: {
+              fields: ["url", "name"],
+            },
+          };
+        }
+        return {
+          ...a,
+          [v]: {
+            populate: "*",
+          },
+        };
+      }, {}),
+    };
+
+    const res = await super.find(ctx);
+    return res;
+  },
+}));
