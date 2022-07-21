9e1567e96e180f7dafed709f57d95667cfa32287
Chupryna Dmytro <dchup@softserveinc.com>
2022-07-21 11:45:48 +0300

    add populate museum-history

--- src/api/museum-history/controllers/museum-history.js
+++ src/api/museum-history/controllers/museum-history.js
@@ -1,9 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  *  museum-history controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::museum-history.museum-history');
+module.exports = createCoreController(
+  "api::museum-history.museum-history",
+  () => ({
+    async find(ctx) {
+      const populate = ["pageStructure"];
+      ctx.query = {
+        ...ctx.query,
+        populate: populate.reduce((a, v) => {
+          if (v === "pageStructure") {
+            return {
+              ...a,
+              [v]: {
+                populate: "*",
+              },
+            };
+          }
+
+          return {
+            ...a,
+            [v]: {
+              populate: "*",
+            },
+          };
+        }, {}),
+      };
+
+      const res = await super.find(ctx);
+      return res;
+    },
+  })
+);
