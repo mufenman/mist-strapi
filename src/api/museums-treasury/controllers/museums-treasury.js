78ede4fd8cc8b896b8585bf8503dc5a3aa4e37a0
Chupryna Dmytro <dchup@softserveinc.com>
2022-07-24 14:38:33 +0300

    museums-treasury

--- src/api/museums-treasury/controllers/museums-treasury.js
+++ src/api/museums-treasury/controllers/museums-treasury.js
@@ -1,9 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  *  museums-treasury controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::museums-treasury.museums-treasury');
+module.exports = createCoreController(
+  "api::museums-treasury.museums-treasury",
+  () => ({
+    async find(ctx) {
+      const populate = ["pageStructure"];
+      ctx.query = {
+        ...ctx.query,
+        populate: populate.reduce((a, v) => {
+          if (v === "pageStructure") {
+            return {
+              ...a,
+              [v]: {
+                populate: "*",
+              },
+            };
+          }
+
+          return {
+            ...a,
+            [v]: {
+              populate: "*",
+            },
+          };
+        }, {}),
+      };
+
+      const res = await super.find(ctx);
+      return res;
+    },
+  })
+);
